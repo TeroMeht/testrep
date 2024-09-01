e401b531b2b24cbca07b2141a04b6ac1043e8c4b
TeroMeh <tero70@hotmail.com>
2024-09-01 10:52:21 +0300

    latest

---
+++ nextjs-dashboard/app/dashboard/portfolio/page.tsx
@@ -0,0 +1,32 @@
+import Table from '@/app/ui/portfolio/table';
+import { lusitana } from '@/app/ui/fonts';
+import { Suspense } from 'react';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { fetchInvoicesPages } from '@/app/lib/data';
+ 
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+  
+  const totalPages = await fetchInvoicesPages(query);
+
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Positions</h1>
+      </div>
+
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense>
+
+    </div>
+  );
+}
